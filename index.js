f2d25c4be1298b0a33d36b2bc5c2c8eb87986f1c
Tim <timtran007@gmail.com>
2022-09-20 22:07:46 -0500

    commit tree d51102acf706330692724068f70c4e989ef5f3a9

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 2
+let num2 = 31
+
+let multiply = num1 * num2
+
+let random = Math.ceil((Math.random() * 10));
+
+let num3 = 10
+let num4 = 6
+
+let mod = num3 % num4
+
+let max = Math.max(0, 5, 10, 15, 20)
 
 // Write your code here
